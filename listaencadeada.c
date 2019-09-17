a132135d217b1a697f0095ea803e57d2bb4135fa
realvesd <43302742+realvesd@users.noreply.github.com>
2019-09-17 19:59:49 -0300

    Update listaencadeada.c

--- listaencadeada.c
+++ listaencadeada.c
@@ -42,6 +42,23 @@ int getTamanho(t_lista lista) {
 //Inserir um elemento na lista
 int inserir(t_lista *lista, int pos, t_elemento dado) {
     t_no * p, * novo;
+// Obter no da lista
+    t_no * getNo(t_lista lista, int pos) {
+    // Retorna 0 se posicao invalida. Do contrario, retorna o elemento
+    int n = 0;
+
+    if (pos<0)
+        return 0; // erro: posicao invalida
+
+    while (lista != NULL) {
+        if (n==pos)
+            return lista;
+        lista = lista->prox;
+        n++;
+    }
+    return 0; // erro: posicao invalida
+}
+
 
     // inserção na primeira posicao ou em lista vazia
     if (pos == 0) {
@@ -52,13 +69,33 @@ int inserir(t_lista *lista, int pos, t_elemento dado) {
         novo->prox = *lista;
         *lista = novo;
         return 1;
+            p = getNo(*lista, pos-1);
+    if (p == NULL)
+         return 0; // erro: posicao invalida 
+    novo = criaNo();
+    if (novo == NULL)
+        return 0; // erro: memoria insuficiente
+    novo->dado = dado;
+    novo->prox = p->prox;
+    p->prox = novo;
+
+    return 1;
+
     }
 
 int main(void) {
 
-int pos;
-t_elemento N;
-t_nomes elem;
+int i;
+t_lista lNomes;
+t_elemento dado;
+    
+for (i = 0; i < 3; i++) { //criando loop para pegar os 3 nomes
+    printf("Digite um nome\n");
+    scanf("%s", &dado.nome);
+    (if (!inserir(&lNomes, i, dado)) { 
+        printf("Erro\n");
+    }
+    
 
 criaNo();
 
