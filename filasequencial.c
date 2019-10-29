69abb031b153ea9ecfdd1eb3b37dc7e1737302fc
realvesd <43302742+realvesd@users.noreply.github.com>
2019-10-28 21:05:13 -0300

    Update filasequencial.c

--- filasequencial.c
+++ filasequencial.c
@@ -1,3 +1,80 @@
+#include <stdio.h>
+#define MAX 5
+
+// Tipo base dos elementos da fila 
+typedef struct elementos {
+    char nome[50];	
+} t_elemento;
+
+typedef struct fila {
+    t_elemento vetor[MAX]; // vetor que armazena a fila
+    int inicio; // posicao do primeiro elemento
+    int fim;    // posicao do ultimo elemento
+    int quant_element;    // numero de elementos da fila
+} t_fila;
+
+t_fila criar()
+{
+    t_fila fila;
+
+    fila.inicio = 0;
+    fila.fim = -1;
+    fila.quant_element = 0;
+
+
+    return fila;
+}
+
+int isVazia (t_fila * fila)
+{
+    return (fila->quant_element == 0);
+}
+
+int isCheia(t_fila * fila)
+{
+    return (fila->quant_element == MAX);
+}
+
+int inserir (t_fila * fila, t_elemento valor)
+{
+    if (isCheia(fila))
+        return 0;
+
+    (fila->quant_element)++;
+    fila->fim = (fila->fim + 1) % MAX;
+    fila->vetor[fila->fim] = valor;
+    return 1;
+}
+
+t_elemento remover(t_fila * fila)
+{
+    t_elemento valor = { "" } ;
+    if (isVazia(fila))
+        return valor; // Erro: fila vazia
+
+    valor = fila->vetor[fila->inicio];
+    fila->vetor[fila->inicio].nome[0] = '\0';// zera, opcional
+    (fila->quant_element)--;
+    fila->inicio = (fila->inicio + 1) % MAX;
+    return valor;
+}
+
+void exibir(t_fila * fila) {
+    int i;
+    if (isVazia(fila)) {
+        printf("Fila vazia\n");
+        return;
+    }
+
+    printf("\nExibindo fila:\n");
+    printf("inicio: %d\n", fila->inicio);
+    printf("fim:    %d\n", fila->fim);
+    for (i=0 ; i<MAX ; i++) {
+        printf("%d::%s\n", i, fila->vetor[i].nome);
+    }
+}
+
+
 int main(void) {
   //criar fila
   t_fila Filinha;
@@ -31,3 +108,20 @@ int main(void) {
 
  //esvaziar a fila
  remover(&Filinha);
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
